1aed6cfe86a1e20ed2d80dfd1eadad0c21b365ea
ftvalentini <ft.valentini@gmail.com>
2020-08-20 23:55:00 -0300

    make cooc matrix creation more efficient

--- metrics/utils/coocurrence.py
+++ metrics/utils/coocurrence.py
@@ -9,7 +9,7 @@ def create_cooc_matrix(document, word_list, str2idx, window_size=8):
     It assumes data has no punctuation
     """
     V = max(str2idx.values())
-    C = scipy.sparse.lil_matrix((V+1,V+1), dtype=np.float32)
+    C = scipy.sparse.dok_matrix((V+1,V+1), dtype=np.float32)
     tokens = document.split()
     for i, word_i in enumerate(tokens):
         if word_i in word_list:
@@ -18,7 +18,7 @@ def create_cooc_matrix(document, word_list, str2idx, window_size=8):
             for j in range(window_start, window_end):
                 if (tokens[j] in word_list) & (i != j):
                     C[str2idx[word_i], str2idx[tokens[j]]] += 1
-    return C.tocsr()
+    return C.tocoo().tocsr()
 
 
 def create_cooc_matrix2(document, word_list, str2idx, window_size=8):
@@ -30,7 +30,7 @@ def create_cooc_matrix2(document, word_list, str2idx, window_size=8):
     de window_size palabras mas atras
     """
     V = max(str2idx.values())
-    C = scipy.sparse.lil_matrix((V+1,V+1), dtype=np.float32)
+    C = scipy.sparse.dok_matrix((V+1,V+1), dtype=np.float32)
     tokens = document.split()
     l1 = 0  # position of center word
     l2 = 0  # position of context word
@@ -47,7 +47,7 @@ def create_cooc_matrix2(document, word_list, str2idx, window_size=8):
             if (token_i in word_list) and (token_j in word_list):
                 C[str2idx[token_i], str2idx[token_j]] += 1
                 C[str2idx[token_j], str2idx[token_i]] += 1
-    return C.tocsr()
+    return C.tocoo().tocsr()
 
 
 ### TEST: matrix creation efficiency
