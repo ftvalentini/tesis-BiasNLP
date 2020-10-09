6ec8b0bbb569a14daa63042032ca1e94d82ed2a2
ftvalentini <ft.valentini@gmail.com>
2020-10-09 20:52:26 -0300

    update plots of bias-frequency by word

--- test_frequency.py
+++ test_frequency.py
@@ -5,13 +5,10 @@ import os, datetime
 import seaborn as sns
 import matplotlib
 import matplotlib.pyplot as plt
-# import scipy.sparse
-# import scipy.stats as stats
-# import nltk
-# from nltk.corpus import stopwords
 
 from scripts.utils.corpora import load_vocab
 from metrics.glove import bias_byword
+
 
 #%% Corpus parameters
 VOCAB_FILE = "embeddings/vocab-C3-V20.txt" # enwiki = C3
@@ -19,38 +16,37 @@ EMBED_FILE = "embeddings/vectors-C3-V20-W8-D1-D100-R0.05-E150-S1.npy" # enwiki =
 
 #%% target pairs parameters
 TARGETS = [
-    ("he", "she")
-    ,("his", "hers")
-    ,("him", "her")
-    ,("himself", "herself")
+    (["he"], ["she"])
+    ,(["his","him"], ["her","hers"])
+    ,(["himself"], ["herself"])
 ]
 
 #%% Load dicts and WE
-str2idx, idx2str, str2count = load_vocab(vocab_file=VOCAB_FILE)
+str2idx, idx2str, str2count = load_vocab(VOCAB_FILE)
 embed_matrix = np.load(EMBED_FILE)
 
 #%% words lists
-words_a = [w[0] for w in TARGETS]
-words_b = [w[1] for w in TARGETS]
+words_a, words_b = zip(*TARGETS)
+words_a = [i for sublist in words_a for i in sublist]
+words_b = [i for sublist in words_b for i in sublist]
 words_context = [w for w in str2count.keys() if w not in words_a + words_b]
 
 #%% WE bias by word
 results = list()
-for words in zip(words_a, words_b):
+for targets in TARGETS:
     df_bias = bias_byword(
-        embed_matrix, [words[0]], [words[1]], words_context, str2idx, str2count)
+        embed_matrix, targets[0], targets[1], words_context, str2idx, str2count)
     results.append(df_bias)
-
 
 #%% Correlation between pairs
 
-gdat = results[0][['freq']]
-for i, words in enumerate(zip(words_a, words_b)):
-    colname = f'rnd_{words[0]}_{words[1]}'
-    gdat.loc[:,colname] = results[i].loc[:,'rel_norm_distance']
 ### check que dframes estan ordenados
-# np.all(results[0]['idx'] == results[1]['idx'])
+assert np.all(results[0]['idx'] == results[1]['idx'])
 ###
+gdat = results[0][['freq']]
+for i, targets in enumerate(TARGETS):
+    colname = f'rnd_{"-".join(targets[0])}_{"-".join(targets[1])}'
+    gdat.loc[:,colname] = results[i].loc[:,'rel_norm_distance']
 
 features = [c for c in gdat.columns if c not in ['idx','word','freq']]
 n_features = len(features)
@@ -86,10 +82,12 @@ for i in range(len(results)):
     axs[i].axhline(0, ls='--', color='black', linewidth=0.5)
     axs[i].set_xlabel('freq')
     axs[i].set_ylabel('rel_norm_distance')
-    w1 = words_a[i]
-    w2 = words_b[i]
-    freq_ratio = np.round(str2count[w1] / str2count[w2], 2)
-    axs[i].set_title(f'{w1}-{w2} (freq. ratio = {freq_ratio})')
+    count1 = sum([str2count[w] for w in TARGETS[i][0]])
+    count2 = sum([str2count[w] for w in TARGETS[i][1]])
+    freq_ratio = np.round(count1 / count2, 2)
+    name1 = "-".join(TARGETS[i][0])
+    name2 = "-".join(TARGETS[i][1])
+    axs[i].set_title(f'{name1}_{name2} (freq. ratio = {freq_ratio})')
     # cbar = plt.colorbar(scatter, ax=ax)
     # cbar.ax.set_title('freq.')
 fig.savefig('results/plots/scatter_pronounpairs_freq_rnd.png', dpi=400)
