5d2ee0703a2c0180823d4559a84e86be31e95297
ftvalentini <ft.valentini@gmail.com>
2020-10-30 10:52:05 -0300

    update biasbyword WE with multiple corpora

--- scripts/05-biasbyword_multiple_we.sh
+++ scripts/05-biasbyword_multiple_we.sh
@@ -2,7 +2,16 @@
 TARGETA="HE"
 TARGETB="SHE"
 
-# iterate over corpora ids
+# convert glove matrices from .bin to .npy
+for i in $(seq 4 8); do
+  VOCABFILE="embeddings/vocab-C$i-V20.txt"
+  EMBEDFILE="embeddings/vectors-C$i-V20-W8-D1-D100-R0.05-E150-S1.bin"
+  OUTFILE="embeddings/glove-C$i-V20-W8-D1-D100-R0.05-E150-S1.npy"
+  python3 -u scripts/02-build_glove_matrix.py \
+    -v $VOCABFILE -e $EMBEDFILE -o $OUTFILE
+done
+
+# compute cosine bias for glove and w2v
 for i in $(seq 4 8); do
   VOCABFILE="embeddings/vocab-C$i-V20.txt"
   # w2v
